40dd95febaccd4095fb126eeace982c0e68ff1e9
TicianoFilho <ticianofilho@gmail.com>
2022-07-25 05:18:22 -0300

    Finalização do CRUD

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,7 +1,7 @@
 const Patient = require('../models/Patient')
-
 const router = require('express').Router()
 
+//salva paciente
 router.post('/', async (req, res) => {
     const {name, healthIsuranceCardId, address, createdAt} = req.body
 
@@ -21,6 +21,7 @@ router.post('/', async (req, res) => {
 
 })
 
+//busca todos os pacientes
 router.get('/', async (req, res) => {
     try {
         const patients = await Patient.find()
@@ -29,6 +30,53 @@ router.get('/', async (req, res) => {
         res.status(500).json({error : error})
     }
 
+})
+
+//busca apenas um paciente
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+
+    try {
+        const patient = await Patient.findById(id)
+        res.status(200).json(patient)
+    } catch (error) {
+        res.status(500).json({error : error}) 
+    }
+
+})
+
+//atualiza parcialmente o paciente
+router.patch('/:id', async (req, res) => {
+
+    const id = req.params.id
+    const {name, healthIsuranceCardId, address, createdAt} = req.body
+    const patient = {
+        name,
+        healthIsuranceCardId,
+        address,
+        createdAt
+    }
+
+    try {
+        const updatedPatient = await Patient.updateOne({_id : id}, patient)
+        if (updatedPatient.matchedCount === 0) {
+            res.status(422).json({message: 'Paciente não encontrado.'})
+        }
+        res.status(200).json({patient})
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+//remove o paciente
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const patient = await Patient.findByIdAndDelete(id)
+        res.status(200).json({message: `Paciente de id ${id} foi removido com sucesso.`})
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
 })
 
 module.exports = router
